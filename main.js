3b926bee5e370e373b8f904ae56d646274b77038
padnitri <57793978+padnitri@users.noreply.github.com>
2021-02-21 20:50:41 +0700

    [Done] all work

--- main.js
+++ main.js
@@ -1,50 +1,66 @@
 const app = Vue.createApp({
     data() {
         return {
+            searchQuery: null,
             msg: 'Hello World!',
             show: false,
 
-            toggle(){
+            toggle() {
                 this.show = !this.show
             },
-            
-            lists: [
-                { name: "Chiling People", image: './images/people_chiling.jpg', isFav: false, done: false },
-                { name: "Dark Room", image: './images/room.jpg', isFav: false, done: false },
-                { name: "Lonely Woman", image: './images/woman.jpg', isFav: false, done: false }
-            ],
-            inputSearch: '',
-            
 
+            lists: [
+                { name: "Chiling People", image: './images/people_chiling.jpg', isFav: false, done: false,isHidden: false },
+                { name: "Dark Room", image: './images/room.jpg', isFav: false, done: false,isHidden: false },
+                { name: "Lonely Woman", image: './images/woman.jpg', isFav: false, done: false,isHidden: false }
+            ],
+            keyword: '',
             favCount: 0,
+            isFound: true,
+            isShowSearchBox: false,
+            isShowImg : false,
+
+            currentImg: './images/people_chiling.jpg'
         }
     },
     methods: {
-        // addNewTask() {
-        //     if (this.inputTask) {
-        //         let newTask = { title: this.inputTask, done: false }
-        //         this.tasks.push(newTask)
-        //         this.inputTask = ''
-        //     }
-        // },
-
-
-
-        searching() {
-            if (this.inputSearch){
-                let searchResult;
-                let search = this.inputSearch
-                if (search == this.lists.name){
-                    return searchResult = true;
-                }
-            }
-
-
-        }
-        ,
         // toggleDone(index){
         //     this.lists[index].done = !this.lists[index].done
         // },
+
+        showSearchBox() {
+            this.isShowSearchBox = !this.isShowSearchBox;
+        },
+        Search() {
+            if (this.keyword) {
+                for (let i = 0; i < this.lists.length; i++) {
+
+                    if (this.lists[i].name === this.keyword) {
+                        this.lists[i].isHidden = false;
+                    } else {
+                        this.lists[i].isHidden = true;
+                    }
+                }
+                if (this.lists.every(element => element.isHidden)) this.isFound = false;
+                this.keyword ='';
+
+
+            }
+        },
+        Cancel() {
+            this.isShowSearchBox = !this.isShowSearchBox;
+            for (const element of this.lists) {
+                element.isHidden = false
+            }
+            this.isFound = true
+        },
+        Close() {
+            this.isShowImg = false;
+        },
+        handleClickImg(index) {
+            this.currentImg = this.lists[index].image;
+            this.isShowImg = true;
+        },
         updateFav(index) {
 
             this.lists[index].isFav = !this.lists[index].isFav
@@ -59,6 +75,9 @@ const app = Vue.createApp({
     computed: {
         calculate() {
             return this.lists.length
+        },
+        filteredSearch(search) {
+            return this.lists.filter(list => search === this.lists.name)
         }
     }
 })
